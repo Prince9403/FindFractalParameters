f25ca30420c2dce6a14991d3ab340148bc5e4689
Prince9403 <vasily.kuznets@gmail.com>
2025-11-29 16:58:39 +0200

    fix: some fixes

--- fractals_dataset.py
+++ fractals_dataset.py
@@ -1,9 +1,14 @@
 import os
 import json
-import matplotlib.pyplot as plt
+from PIL import Image
+
 import numpy as np
 import torch
 from torch.utils.data import Dataset
+from torchvision import transforms
+
+model_mean = [0.485, 0.456, 0.406]
+model_std = [0.229, 0.224, 0.225]
 
 
 class FractalImageadDataset(Dataset):
@@ -12,12 +17,19 @@ class FractalImageadDataset(Dataset):
             self.dict_fractals = json.load(fd)
         self.list_fnames = list(self.dict_fractals.keys())
         self.img_dir = images_folder
+        self.transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(model_mean, model_std)
+        ])
 
     def __len__(self):
         return len(self.list_fnames)
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.list_fnames[idx])
-        image = plt.imread(img_path)
+        image = Image.open(img_path)
+        image_torch = self.transform(image)
+
         c0, c1 = self.dict_fractals[self.list_fnames[idx]]
-        return image, torch.from_numpy(np.array([c0, c1]))
+        return image_torch, torch.from_numpy(np.array([c0, c1], dtype=np.float32))
--- train_neural_network.py
+++ train_neural_network.py
@@ -1,6 +1,6 @@
+import datetime
 import os
 
-import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
@@ -9,12 +9,14 @@ def train_nn(neural_network, train_dataset, validation_dataset, optimizer, batch
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
 
+    os.makedirs(save_model_folder, exist_ok=True)
+
     mse_loss = torch.nn.MSELoss()
 
     train_losses = []
     val_losses = []
 
-    for epoch_num in range(num_epochs):
+    for epoch_num in range(1, num_epochs + 1):
         total_train_loss = 0.0
         total_val_loss = 0.0
         num_train_batches = 0.0
@@ -27,12 +29,13 @@ def train_nn(neural_network, train_dataset, validation_dataset, optimizer, batch
             batch_loss = mse_loss(out_tensor, coeffs)
 
             optimizer.zero_grad()
-            batch_loss.backward(
-            optimizer.step())
+            batch_loss.backward()
+            optimizer.step()
 
             total_train_loss += batch_loss.item()
             num_train_batches += 1
-        train_losses.append(total_train_loss / num_train_batches)
+        avg_train_loss = total_train_loss / num_train_batches
+        train_losses.append(avg_train_loss)
 
         if epoch_num % saving_freq == 0:
             save_model_path = os.path.join(save_model_folder, save_model_name + f"_epoch__{epoch_num}.ckpt")
@@ -46,7 +49,10 @@ def train_nn(neural_network, train_dataset, validation_dataset, optimizer, batch
 
             total_val_loss += batch_loss.item()
             num_val_batches += 1
-        val_losses.append(total_val_loss / num_val_batches)
+        avg_val_loss = total_val_loss / num_val_batches
+        val_losses.append(avg_val_loss)
+
+        print(f"{datetime.datetime.now()} Epoch {epoch_num}, train loss {avg_train_loss}, val loss {avg_val_loss}")
 
     return train_losses, val_losses
 
